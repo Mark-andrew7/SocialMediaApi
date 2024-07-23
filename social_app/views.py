0273c19a8ced97eb77c00d106c9a87ca20c26cb9
Mark-andrew7 <kamaumark19@gmail.com>
2024-07-23 16:05:21 +0300

    Set the login logic Added style to the display message once a user provides invalid credentials

--- social_app/views.py
+++ social_app/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, redirect
-from django.http import JsonResponse
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 
@@ -32,7 +31,20 @@ def signup(request):
   
 
 def login(request):
-  return render(request, 'login.html')
+  if request.method == 'POST':
+    username = request.POST['username']
+    password = request.POST['password']
+
+    user = auth.authenticate(username=username, password=password)
+
+    if user is not None:
+      auth.login(request, user)
+      return redirect('/')
+    else:
+      messages.info(request, 'Invalid credentials')
+      return redirect('login')
+  else:
+    return render(request, 'login.html')
 
 def logout(request):
   return redirect('index')
